0f940d631c70a29db895e55749637bf0bbe86c0c
Akihitori <akihitoribara@gmail.com>
2026-02-22 11:25:58 +0530

    Database: Uploaded Migration scripts for Users table

---
+++ invoice-details/Migrations/20260222053939_AddUserTableV2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace invoice_details.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTableV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Invoices",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "bytea", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "bytea", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_UserId",
+                table: "Invoices",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoices_Users_UserId",
+                table: "Invoices",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoices_Users_UserId",
+                table: "Invoices");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_UserId",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "Deadline",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Invoices");
+        }
+    }
+}
